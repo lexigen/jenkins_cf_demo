881937e5eec4f6d0650799d6cd14f454ca640043
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2014-07-14 19:19:09 +0300

    Register app start time

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+ENV['DTIME'] = Time.now.to_s
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
